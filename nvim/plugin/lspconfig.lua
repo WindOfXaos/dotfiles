200eb68cbb949ed370bdcaaffba2d15cdbb56577
WindOfXaos <mouaz30970893@f-eng.tanta.edu.eg>
2022-02-10 23:33:07 +0200

    add cmake language server

--- nvim/plugin/lspconfig.lua
+++ nvim/plugin/lspconfig.lua
@@ -59,6 +59,7 @@ sources = cmp.config.sources({
 
 -- Setup lspconfig.
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+require'lspconfig'.cmake.setup{}
 require'lspconfig'.pyright.setup{}
 require'lspconfig'.vimls.setup{}
 require'lspconfig'.clangd.setup{
--- nvim/plugin/nvim-tree.lua
+++ nvim/plugin/nvim-tree.lua
@@ -21,7 +21,7 @@ require'nvim-tree'.setup {
   ignore_ft_on_setup  = {},
   auto_close          = true,
   open_on_tab         = false,
-  hijack_cursor       = false,
+  hijack_cursor       = true,
   update_cwd          = false,
   update_to_buf_dir   = {
     enable = true,
